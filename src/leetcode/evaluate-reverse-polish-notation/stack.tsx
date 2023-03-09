57c40b3c93f6c452bb9322a320605f7d6292364d
Near Huscarl <near.huscarl@gmail.com>
2023-03-09 23:19:11 +0700

    add erpn visualization

--- src/leetcode/evaluate-reverse-polish-notation/stack.tsx
+++ src/leetcode/evaluate-reverse-polish-notation/stack.tsx
@@ -1,10 +1,48 @@
 import React from 'react';
-import { useVisualizerData, VC, VArray, VLookupTable } from 'visualizer';
+import { useVisualizerData, VC, VArray, VStack } from 'visualizer';
 import { red, amber, lightGreen, blue } from 'colors';
 
 export const Visualizer = () => {
   const { data, type, expression } = useVisualizerData();
-  const {} = data;
+  const { stack, tokens = [], i } = data;
+  const operators = new Set(['+', '-', '*', '/']);
+  const test = type === 'testExpressionSuccess';
 
-  return null;
+  return (
+    <>
+      <VArray
+        value={tokens}
+        pointers={[{ name: 'i', value: i, color: red[500] }]}
+        getElementStyle={(e, i, style) => {
+          if (operators.has(e + '')) {
+            style.background = amber[100];
+          }
+
+          return style;
+        }}
+      />
+      <VStack
+        y={VC.Array.ItemSize + 40}
+        value={stack}
+        length={tokens.filter((t) => !operators.has(t + '')).length}
+        getElementStyle={(value, i, style) => {
+          if (
+            (expression === "token === '+'" ||
+              expression === "token === '-'" ||
+              expression === "token === '*'" ||
+              expression === "token === '/'") &&
+            test
+          ) {
+            if (i === stack.length - 1 || i === stack.length - 2) {
+              style.background =
+                i % 2 === 0 ? lightGreen[300] : lightGreen[500];
+              style.color = '#ffffff';
+            }
+          }
+
+          return style;
+        }}
+      />
+    </>
+  );
 };
