f9a455f7467a5d67933c7f413adacd35d83e4147
Near Huscarl <near.huscarl@gmail.com>
2023-04-05 21:39:49 +0700

    add remove-duplicates-from-sorted-array visualizer

--- src/leetcode/remove-duplicates-from-sorted-array/twoPointers.tsx
+++ src/leetcode/remove-duplicates-from-sorted-array/twoPointers.tsx
@@ -1,12 +1,47 @@
 import React from 'react';
-import { useVisualizerData, VArray, VTree } from 'visualizer';
-import { red, green, amber, lightGreen, teal, transform } from 'colors';
-
-const interpolateColor = transform([1, 6], [amber[200], amber[900]]);
+import { useVisualizerData, VArray, VField } from 'visualizer';
+import {
+  red,
+  indigo,
+  amber,
+  pink,
+  lightGreen,
+  deepPurple,
+  transform,
+} from 'colors';
 
 export const Visualizer = () => {
   const { data, expression, type } = useVisualizerData();
-  const {} = data;
+  const { left, right, nums = [] } = data;
+  const range = [nums.at(0), nums.at(nums.length / 2), nums.at(-1)];
+  const colorArr = [red[300], amber[300], lightGreen[300]];
+  const interpolateColor = transform(range, colorArr);
+  const test = type === 'testExpressionSuccess';
 
-  return null;
+  return (
+    <>
+      <VField y={0} label="k" value={left} />
+      <VArray
+        y={50}
+        value={nums}
+        highlightRange={right === nums.length && [0, left - 1, pink[700]]}
+        pointers={[
+          { name: 'left', value: left, color: indigo[500] },
+          { name: 'right', value: right, color: deepPurple[500] },
+        ]}
+        getElementStyle={(value, i, style) => {
+          style.background = interpolateColor(value);
+
+          if (
+            expression === 'nums[right] !== nums[right - 1]' &&
+            (i === right || i === right - 1)
+          ) {
+            style.background = test ? lightGreen[500] : red[500];
+          }
+
+          return style;
+        }}
+      />
+    </>
+  );
 };
