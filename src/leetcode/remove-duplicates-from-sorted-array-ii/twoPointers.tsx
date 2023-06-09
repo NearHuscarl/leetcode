d32e6873391b0596adfddb8b0c4c1ced6d6f1633
nk-nguyen <107663809+nk-nguyen@users.noreply.github.com>
2023-06-09 19:59:29 +0700

    add visualizer for remove-duplicates-from-sorted-array-ii

--- src/leetcode/remove-duplicates-from-sorted-array-ii/twoPointers.tsx
+++ src/leetcode/remove-duplicates-from-sorted-array-ii/twoPointers.tsx
@@ -4,7 +4,7 @@ import {
   red,
   indigo,
   amber,
-  pink,
+  blue,
   lightGreen,
   deepPurple,
   transform,
@@ -12,4 +12,26 @@ import {
 
 export const Visualizer = () => {
   const { data, expression, type } = useVisualizerData();
+  const { nums, left, right } = data;
+  const test = type === 'testExpressionSuccess';
+
+  return (
+    <VArray
+      value={nums}
+      pointers={[
+        { name: 'left', value: left, color: indigo[500] },
+        { name: 'right', value: right, color: amber[500] },
+      ]}
+      getElementStyle={(v, i, style) => {
+        if (
+          expression === 'nums[right] === nums[right + 1]' &&
+          (i === right || i === right + 1)
+        ) {
+          style.background = test ? lightGreen[500] : red[500];
+        }
+
+        return style;
+      }}
+    />
+  );
 };
